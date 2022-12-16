0cbff124b64a9f7bf5d19a800dcb46f97656b555
theliftus <omofunmilara@gmail.com>
2022-12-16 05:42:32 -0800

    1isdigit c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,12 +6,10 @@
  */
 int _isdigit(int c)
 {
-	int num;
 
-	for (num = 0; num <= 9; num++)
-	{
-	if (num == c)
-	return (1);
-	}
-	return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	else
+		return (0);
 }
