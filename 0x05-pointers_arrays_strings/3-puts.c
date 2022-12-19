29afefcf48faca1936830b29afaab0e05350d8dc
theliftus <omofunmilara@gmail.com>
2022-12-19 07:17:54 -0800

    3puts c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - A function that prints a tring followed by a new line.
+ * @str: input string
+ * Return: no return
+ */
+
+void _puts(char *str)
+{
+	int count;
+
+	for (count = 0; str[count] != '\0'; count++)
+	{
+	_putchar(str[count]);
+	}
+	_putchar('\n');
+}
