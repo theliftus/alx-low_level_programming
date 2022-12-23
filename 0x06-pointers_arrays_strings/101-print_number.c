b7a7606f6c4f94511c84f8f74ba796985239b08e
theliftus <omofunmilara@gmail.com>
2022-12-23 12:38:14 -0800

    101print numbercc

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,20 +3,24 @@
 /**
  * print_number - Prints an integer.
  * @n: The integer to be printed.
+ * Return: 0
  */
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int n1;
+
+	n1 = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		n1 = -n;
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
-}
+	if ((n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+	}
