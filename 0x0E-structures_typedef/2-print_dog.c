9f2907bdf449448814b452bac24ebc7e9ff18e49
theliftus <omofunmilara@gmail.com>
2023-01-17 05:09:10 -0800

    printing struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,29 +6,27 @@
  * print_dog - prints the struct dog
  * @d: the memeber to be printed
  *
- * Description: it writes a function thta prints
+ * Return: void
  */
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
-	{
+		return;
+
 		if (d->name == NULL)
-		{
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
-		}
+
 		if (d->age < NULL)
-		{
-			print("Age: (nil)\n");
+			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", d->age);
-		}
+
 		if (d->owner == NULL)
 		{
 			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", d->owner);
 		}
-	}
 }
